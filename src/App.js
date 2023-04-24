182d8c26f80e9b189de4367617e9abc1f99c144c
shalu-tulsyan <shalutulsyan08@gmail.com>
2023-04-24 23:14:37 +0530

    implemented geolocation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import './App.css';
 
 function App() {
+
+  const [location, setLocation] = useState();
+  const [isMap, setIsMap] = useState(false);
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  });
+
+  function success(position) {
+    setLocation(position.coords)
+    console.log(position.coords);
+  }
+
+  function error(error) {
+    console.log(error)
+  }
+
+  function handleClick() {
+    if (navigator.geolocation)
+    {
+      navigator.geolocation.getCurrentPosition(success,error);
+    } 
+  }
+
+  function handleMapClick() {
+    setIsMap(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Geolocation in React</h1>
+        <button onClick={handleClick}>Get the Location</button>
+        {location && 
+          <>
+          <p>The information of your location is: latitude {location.latitude} and longitude { location.longitude}</p>
+            <div className='mapButtonContainer'>
+              <button onClick={handleMapClick}>Show Location on Map</button>
+            </div> 
+            {isMap &&
+              <div className='mapContainer'>
+                {!isLoaded ? (
+                  <h1>Loading...</h1>
+                ) : (
+                <GoogleMap
+                  mapContainerClassName="map"
+                  center={{ lat: location.latitude, lng: location.longitude }}
+                  zoom={10}
+                >
+                   <Marker position={{ lat: location.latitude, lng: location.longitude }} /> 
+                </GoogleMap>
+      )}
+              </div>
+            }
+          </>
+        }
+      </div>
+    </>
   );
 }
 
